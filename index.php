dc65c969166dd1b5504c5d3016a7288aaa019957
MatteoGaloni <mtgaloni@gmail.com>
2023-06-09 16:35:50 +0200

    add functions file

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
+include("./functions.php");
+
 $pswLength = ($_GET["password"]);
 
-function randomPsw($number)
-{
-    $alphaNumericArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'A', 'b', 'B', 'c', 'C'];
-    $result = "";
-    for ($i = 0; $i < $number; $i++) {
-        $RandNumber = rand(0, count($alphaNumericArray) - 1);
-        $result .= $alphaNumericArray[$RandNumber];
-    }
-    return $result;
-}
 
 
 
@@ -35,7 +27,7 @@ function randomPsw($number)
             <form method="GET" action="index.php">
                 <div class="mb-3">
                     <label for="password" class="form-label">Password Generator</label>
-                    <input name="password" min="5" max="15" type="number" class="form-control" id="password" aria-describedby="emailHelp" placeholder="Inserisci un numero in base alla lunghezza della password desiderata">
+                    <input name="password" value="<?php echo $pswLength ?>" min="5" max="15" type="number" class="form-control" id="password" aria-describedby="emailHelp" placeholder="Inserisci un numero in base alla lunghezza della password desiderata">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 <button type="reset" class="btn btn-primary">Reset</button>
