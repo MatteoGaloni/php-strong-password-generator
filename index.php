3687cf166b72e9368eb9351b74e09c052b5b7e62
MatteoGaloni <mtgaloni@gmail.com>
2023-06-09 15:43:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <form method="POST" action="index.php">
+                <div class="mb-3">
+                    <label for="password" class="form-label">Email address</label>
+                    <input name="password" type="number" class="form-control" id="password" aria-describedby="emailHelp">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+        </div>
+    </main>
+</body>
+
+</html>
